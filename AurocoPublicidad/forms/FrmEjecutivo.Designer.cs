b60f76816d34ded6efd275afbdd447b3cc83d56f
Victor Jimenez <vjimenez@wigilabs.com>
2024-04-19 16:23:54 -0500

    auro

---
+++ AurocoPublicidad/forms/FrmEjecutivo.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace AurocoPublicidad.forms
+{
+    partial class FrmEjecutivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.Código = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.DgMedios = new System.Windows.Forms.DataGridView();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fcreacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgMedios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.Código);
+            this.groupBox1.Controls.Add(this.Label1);
+            this.groupBox1.Controls.Add(this.Label2);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Location = new System.Drawing.Point(27, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(706, 169);
+            this.groupBox1.TabIndex = 42;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(442, 114);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 40);
+            this.btnReset.TabIndex = 62;
+            this.btnReset.Text = "&Resetear";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Transparent;
+            this.btnGuardar.Location = new System.Drawing.Point(549, 114);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(116, 40);
+            this.btnGuardar.TabIndex = 61;
+            this.btnGuardar.Text = "&Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // Código
+            // 
+            this.Código.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Código.Location = new System.Drawing.Point(32, 24);
+            this.Código.Name = "Código";
+            this.Código.Size = new System.Drawing.Size(58, 16);
+            this.Código.TabIndex = 55;
+            this.Código.Text = "Código:";
+            // 
+            // Label1
+            // 
+            this.Label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(20, 51);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(63, 16);
+            this.Label1.TabIndex = 56;
+            this.Label1.Text = "Nombre:";
+            // 
+            // Label2
+            // 
+            this.Label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(46, 83);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(37, 21);
+            this.Label2.TabIndex = 57;
+            this.Label2.Text = "DNI:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtCodigo.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(90, 21);
+            this.txtCodigo.MaxLength = 6;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.ReadOnly = true;
+            this.txtCodigo.Size = new System.Drawing.Size(104, 21);
+            this.txtCodigo.TabIndex = 49;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNombre.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(89, 51);
+            this.txtNombre.MaxLength = 180;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(464, 21);
+            this.txtNombre.TabIndex = 52;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDescripcion.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(89, 81);
+            this.txtDescripcion.MaxLength = 100;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(147, 21);
+            this.txtDescripcion.TabIndex = 50;
+            // 
+            // DgMedios
+            // 
+            this.DgMedios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgMedios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigo,
+            this.nombre,
+            this.dni,
+            this.usuario,
+            this.fcreacion});
+            this.DgMedios.Location = new System.Drawing.Point(27, 200);
+            this.DgMedios.Name = "DgMedios";
+            this.DgMedios.ReadOnly = true;
+            this.DgMedios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DgMedios.Size = new System.Drawing.Size(704, 213);
+            this.DgMedios.TabIndex = 41;
+            this.DgMedios.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgMedios_CellContentClick);
+            // 
+            // codigo
+            // 
+            this.codigo.HeaderText = "Codigo";
+            this.codigo.Name = "codigo";
+            this.codigo.ReadOnly = true;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            this.nombre.Width = 180;
+            // 
+            // dni
+            // 
+            this.dni.HeaderText = "DNI";
+            this.dni.Name = "dni";
+            this.dni.ReadOnly = true;
+            // 
+            // usuario
+            // 
+            this.usuario.HeaderText = "Usuario";
+            this.usuario.Name = "usuario";
+            this.usuario.ReadOnly = true;
+            // 
+            // fcreacion
+            // 
+            this.fcreacion.HeaderText = "Fecha Creación";
+            this.fcreacion.Name = "fcreacion";
+            this.fcreacion.ReadOnly = true;
+            // 
+            // FrmEjecutivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 438);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.DgMedios);
+            this.Name = "FrmEjecutivo";
+            this.Text = "FrmEjecutivo";
+            this.Load += new System.EventHandler(this.FrmEjecutivo_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgMedios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnGuardar;
+        internal System.Windows.Forms.Label Código;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label Label2;
+        protected System.Windows.Forms.TextBox txtCodigo;
+        internal System.Windows.Forms.TextBox txtNombre;
+        internal System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.DataGridView DgMedios;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dni;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fcreacion;
+    }
+}
---
+++ AurocoPublicidad/forms/FrmEjecutivo.cs
@@ -0,0 +1,67 @@
+﻿using AurocoPublicidad.models.request;
+using AurocoPublicidad.util;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AurocoPublicidad.forms
+{
+    public partial class FrmEjecutivo : Form
+    {
+        public FrmEjecutivo()
+        {
+            InitializeComponent();
+        }
+
+        private void DgMedios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FrmEjecutivo_Load(object sender, EventArgs e)
+        {
+            cargarEjecutivos();
+        }
+
+
+        private async void cargarEjecutivos()
+        {
+            string respuesta = await GetService(Global.servicio + "/api-auroco/ejecutivos");
+            List<models.request.Ejecutivo> lst = JsonConvert.DeserializeObject<List<models.request.Ejecutivo>>(respuesta);
+
+
+
+
+            DgMedios.Rows.Clear();
+
+            foreach (Ejecutivo eje in lst)
+            {
+                int rowIndex = DgMedios.Rows.Add();
+                DgMedios.Rows[rowIndex].Cells["codigo"].Value = eje.C_EJECUTIVO;
+                DgMedios.Rows[rowIndex].Cells["nombre"].Value = eje.NOMBRES;
+                DgMedios.Rows[rowIndex].Cells["dni"].Value = eje.DNI_EJECUTIVO;
+                DgMedios.Rows[rowIndex].Cells["usuario"].Value = eje.USUARIO;
+                DgMedios.Rows[rowIndex].Cells["fcreacion"].Value = eje.F_CREACION;
+
+            }
+        }
+
+        private async Task<string> GetService(string cadena)
+        {
+            WebRequest oRequest = WebRequest.Create(cadena);
+            WebResponse oResponse = await oRequest.GetResponseAsync();
+            StreamReader sr = new StreamReader(oResponse.GetResponseStream());
+            return await sr.ReadToEndAsync();
+        }
+
+    }
+}
