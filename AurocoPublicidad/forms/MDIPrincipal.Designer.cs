ec92ac9cc65b8cce40cc2d8632d26ee0e0aac755
Victor Hugo Jimenez Torero <vjimenez@feban.local>
2025-08-11 17:36:28 -0500

    add

--- AurocoPublicidad/forms/MDIPrincipal.Designer.cs
+++ AurocoPublicidad/forms/MDIPrincipal.Designer.cs
@@ -62,6 +62,7 @@
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contabilidadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.facturaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturasConsultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
@@ -75,7 +76,6 @@
             this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
-            this.facturasConsultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.toolStrip1.SuspendLayout();
@@ -88,8 +88,8 @@
             this.operacionesToolStripMenuItem,
             this.reportesToolStripMenuItem,
             this.windowsMenu,
-            this.helpMenu,
-            this.contabilidadToolStripMenuItem});
+            this.contabilidadToolStripMenuItem,
+            this.helpMenu});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.MdiWindowListItem = this.windowsMenu;
             this.menuStrip.Name = "menuStrip";
@@ -285,7 +285,7 @@
             // 
             this.contentsToolStripMenuItem.Name = "contentsToolStripMenuItem";
             this.contentsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F1)));
-            this.contentsToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.contentsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.contentsToolStripMenuItem.Text = "&Contenido";
             // 
             // indexToolStripMenuItem
@@ -293,7 +293,7 @@
             this.indexToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("indexToolStripMenuItem.Image")));
             this.indexToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Black;
             this.indexToolStripMenuItem.Name = "indexToolStripMenuItem";
-            this.indexToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.indexToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.indexToolStripMenuItem.Text = "&Índice";
             // 
             // searchToolStripMenuItem
@@ -301,18 +301,18 @@
             this.searchToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("searchToolStripMenuItem.Image")));
             this.searchToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Black;
             this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
-            this.searchToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.searchToolStripMenuItem.Text = "&Buscar";
             // 
             // toolStripSeparator8
             // 
             this.toolStripSeparator8.Name = "toolStripSeparator8";
-            this.toolStripSeparator8.Size = new System.Drawing.Size(173, 6);
+            this.toolStripSeparator8.Size = new System.Drawing.Size(177, 6);
             // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.aboutToolStripMenuItem.Text = "&Acerca de... ...";
             // 
             // contabilidadToolStripMenuItem
@@ -330,6 +330,13 @@
             this.facturaciónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.facturaciónToolStripMenuItem.Text = "&Facturar Ordenes";
             this.facturaciónToolStripMenuItem.Click += new System.EventHandler(this.facturaciónToolStripMenuItem_Click);
+            // 
+            // facturasConsultaToolStripMenuItem
+            // 
+            this.facturasConsultaToolStripMenuItem.Name = "facturasConsultaToolStripMenuItem";
+            this.facturasConsultaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.facturasConsultaToolStripMenuItem.Text = "Facturas Consulta";
+            this.facturasConsultaToolStripMenuItem.Click += new System.EventHandler(this.facturasConsultaToolStripMenuItem_Click);
             // 
             // statusStrip
             // 
@@ -461,13 +468,6 @@
             // 
             this.toolStripMenuItem5.Name = "toolStripMenuItem5";
             this.toolStripMenuItem5.Size = new System.Drawing.Size(32, 19);
-            // 
-            // facturasConsultaToolStripMenuItem
-            // 
-            this.facturasConsultaToolStripMenuItem.Name = "facturasConsultaToolStripMenuItem";
-            this.facturasConsultaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.facturasConsultaToolStripMenuItem.Text = "Facturas Consulta";
-            this.facturasConsultaToolStripMenuItem.Click += new System.EventHandler(this.facturasConsultaToolStripMenuItem_Click);
             // 
             // MDIPrincipal
             // 
