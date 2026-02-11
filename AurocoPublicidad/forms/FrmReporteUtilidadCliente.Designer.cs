cc79d27665f87a683cdfcefe5f85cef5e9bd183f
Victor Hugo Jimenez Torero <vjimenez@feban.local>
2026-02-11 17:02:35 -0500

    add 11-02

--- AurocoPublicidad/forms/FrmReporteUtilidadCliente.Designer.cs
+++ AurocoPublicidad/forms/FrmReporteUtilidadCliente.Designer.cs
@@ -146,6 +146,7 @@
             this.button1.TabIndex = 66;
             this.button1.Text = "ver ruta";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FrmReporteUtilidadCliente
