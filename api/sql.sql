7bf38b745987ec4af5daada28c6dfc9237e63a99
Victor Hugo Jimenez Torero <vjimenez@feban.local>
2026-02-20 17:44:43 -0500

    add

--- api/sql.sql
+++ api/sql.sql
@@ -1,3 +1,3 @@
-SELECT concat(f.serie,f.correlativo) factura,c.c_contrato,agencia,t.ruc,t.RAZON_SOCIAL,f.fecha as F_CREACION,c.C_MONEDA,c.inversion,f.total FROM facturas f,ORD_CONTRATOS c,ORD_CLIENTES t  
+SELECT concat(f.serie,f.correlativo) C_,c.c_contrato,agencia,t.ruc RUC,t.RAZON_SOCIAL,f.fecha as F_CREACION,c.C_MONEDA,c.inversion,f.total,f.estado,f.mensaje FROM facturas f,ORD_CONTRATOS c,ORD_CLIENTES t  
 where CONCAT('T0',f.c_contrato)=c.C_CONTRATO
 and c.C_CLIENTE=t.C_CLIENTE;
